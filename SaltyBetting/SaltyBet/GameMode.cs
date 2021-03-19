d9e7b1b43c0b56f692c56d979ee6e1eed5eebf88
Pheubel <luca.guagliardo@live.nl>
2021-03-19 22:48:54 +0100

    Added documentation for Game modes

--- SaltyBetting/SaltyBet/GameMode.cs
+++ SaltyBetting/SaltyBet/GameMode.cs
@@ -5,9 +5,13 @@
     /// </summary>
     public enum GameMode : byte
     {
+        /// <summary> The game mode is not know at this time.</summary>
         Unknown,
+        /// <summary> Fighters get picked at random for 1 on 1 matches.</summary>
         Matchmaking,
+        /// <summary> Preselected fighters fight 1 on 1 in an elimination style tournament until 1 is left.</summary>
         Tournament,
+        /// <summary> Fights requested by viewers that can star any fighter they choose.</summary>
         Exhibition
     }
 }
